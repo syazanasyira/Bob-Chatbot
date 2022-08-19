a510c5082da79c00b27fb8038b9e526f3df066c0
syazanasyira <53511952+syazanasyira@users.noreply.github.com>
2022-08-19 17:21:43 +0800

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,33 @@
+// Options the user could type in
+const prompts = 
+[
+  ["hi", "hey", "hello", "good morning", "good afternoon", "hai"],
+  ["can i ask you a question", 
+  "are you available right now ?",
+  "can ask question ?", 
+  "can i ask something ?", 
+  "can i ask something "],
+  ["can you be fast"],
+  ["why you so slow"]
+
+]
+
+// Possible responses, in corresponding order
+
+const replies = 
+[
+  ["Hello!", "Hi!"],
+  ["Sure"],
+  ["Whoa, chill out!"],
+  ["Calm down, I know what I'm doing!"],
+  ["Fine. Be that way!"],
+  ["Whatever"]
+]
+
+// Random for any other user input
+
+const alternative = 
+[
+  "Fine. Be that way!",
+  "Whatever"
+]
